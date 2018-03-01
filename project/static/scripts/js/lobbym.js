53ec007f18f25326a52e991dc1ec5286c319adc6
Luke Gerhart <luke_gerhart@hotmail.com>
2018-03-01 10:23:03 -0500

    all users in lobby see new uers joining

--- project/static/scripts/js/lobbym.js
+++ project/static/scripts/js/lobbym.js
@@ -1,10 +1,18 @@
 
 function setup() {
+	console.log(users);
+	for (user in users) {
+		$("#users").append($("<li>").text(users[user]));
+	}
 	var socket = io.connect('http://' + document.domain + ':' + location.port);
 	socket.on('connect', function() {
-		console.log(lobbycode);
-		socket.emit('join', {'code':lobbycode, 'username':username});
+		socket.emit('join', {'code': lobbycode, 'username':username});
+	});
+	
+	socket.on('new user', function(username) {
+		console.log("new user joined");
+		$("#users").append($("<li>").text(username));
 	});
 }
-//window.addEventListener("load", setup, true);
+window.addEventListener("load", setup, true);
 
