3467dcbc8f73b59fb2cf6480e0d31b22beb580f1
Luke Gerhart <luke_gerhart@hotmail.com>
2018-02-28 23:28:21 -0500

    changed such that refreshing page after joining lobby doesnt rejoin

--- project/static/scripts/js/lobbym.js
+++ project/static/scripts/js/lobbym.js
@@ -2,10 +2,9 @@
 function setup() {
 	var socket = io.connect('http://' + document.domain + ':' + location.port);
 	socket.on('connect', function() {
-		var lobbycode = $("#lobbycode").text().substring(5).trim();
 		console.log(lobbycode);
 		socket.emit('join', {'code':lobbycode, 'username':username});
 	});
 }
-window.addEventListener("load", setup, true);
+//window.addEventListener("load", setup, true);
 
