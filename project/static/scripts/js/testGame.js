ddc845198af716be74332e247cc483037e10272b
Tyler Bobik <shed412@gmail.com>
2018-03-02 01:55:04 -0500

    making phaser game

--- project/static/scripts/js/testGame.js
+++ project/static/scripts/js/testGame.js
@@ -1,34 +1,28 @@
 //test game code
-function setupCanvas() {
-	// Create a <canvas> HTML tag
-    canvas = document.createElement( 'canvas' );
-	canvas.width = window.innerWidth;				
-	canvas.height = window.innerHeight;
-	
-	document.body.scrollTop = 0;
-	document.body.style.overflow = 'hidden'; // stackoverflow.com/questions/26745292
-	
-	// Get a CanvasRenderingContext2D on the canvas
-	c = canvas.getContext( '2d' );
-	
-	// Create a <div> HTML tag called container
-	container = document.createElement( 'div' );
-	container.className = "container";
-	
-	// Put the canvas in the container
-	container.appendChild(canvas);
-	// Put the container on the page
-	document.body.appendChild( container );
-}//setupCanvas
 
-function resetCanvas (e) {
- 	// Resize the canvas - but remember - this clears the canvas too
-  	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	
-	document.body.scrollTop = 0;
-	document.body.style.overflow = 'hidden'; 
-	
-	// Scroll to the top left.
-	window.scrollTo(0,0);
+var canvasWidth;
+
+var config = {
+	type: Phaser.AUTO, 
+	width: 800,
+	height: 600,
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+
+}
+
+function create() {
+
+} 
+
+function update() {
+
 }
