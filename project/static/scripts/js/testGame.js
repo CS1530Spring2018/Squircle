85a99c4625f7c0df2194e79c465bfaa32d63f31e
Tyler Bobik <shed412@gmail.com>
2018-03-04 00:53:41 -0500

    added platforms

--- project/static/scripts/js/testGame.js
+++ project/static/scripts/js/testGame.js
@@ -4,7 +4,7 @@
 var baseUrl = "http://127.0.0.1:5000/static/";
 
 var assets = {
-	'image': '/images/gameAssets/test/',
+	'image': 'images/gameAssets/test/',
 }
 
 function url_for(type, name) {
@@ -23,6 +23,13 @@ var config = {
 	type: Phaser.AUTO, 
 	width: 800,
 	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: {y: 300},
+			debug: false
+		}
+	},
 	scene: {
 		preload: preload,
 		create: create,
@@ -34,10 +41,29 @@ var game = new Phaser.Game(config);
 
 function preload() {
 	this.load.image('sky', url_for('image', 'sky.png'));
+	this.load.image('ground', url_for('image', 'platform.png'));
+	this.load.image('star', url_for('image', 'star.png'));
+	this.load.image('bomb', url_for('image', 'bomb.png'));
+	/**
+	 * this loads a sprite sheet
+	 * each sprite frame is 32x48 pixels
+	*/
+	this.load.image('dude', url_for('image', 'dude.png',
+			{frameWidth: 32, frameHeight: 48}));
 }
 
 function create() {
 	this.add.image(400, 300, 'sky');
+
+	var platforms;
+
+	platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update() {
