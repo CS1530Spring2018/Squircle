0aeb00470a186e8304c056171a8bdd4f8d1f864d
Tyler Bobik <shed412@gmail.com>
2018-03-04 02:07:08 -0500

    loading sprite from sprite sheet now

--- project/static/scripts/js/testGame.js
+++ project/static/scripts/js/testGame.js
@@ -6,6 +6,8 @@ var baseUrl = "http://127.0.0.1:5000/static/";
 var assets = {
 	'image': 'images/gameAssets/test/',
 }
+var platforms;
+var player;
 
 function url_for(type, name) {
 	return baseUrl + assets[type] + name;
@@ -48,22 +50,48 @@ function preload() {
 	 * this loads a sprite sheet
 	 * each sprite frame is 32x48 pixels
 	*/
-	this.load.image('dude', url_for('image', 'dude.png',
-			{frameWidth: 32, frameHeight: 48}));
+	this.load.spritesheet('dude', url_for('image', 'dude.png',),
+	{frameWidth: 32, frameHeight: 48});
 }
 
 function create() {
 	this.add.image(400, 300, 'sky');
 
-	var platforms;
-
 	platforms = this.physics.add.staticGroup();
 
+	//this line makes multiple lines spanning the bottom
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+	platforms.create(750, 220, 'ground');
+
+	player = this.physics.add.sprite(100, 450, 'dude');
+
+	player.setBounce(0.2);
+	player.setCollideWorldBounds(true);
+
+	//setting up animation for the character
+	this.anims.create({
+		key: 'left',
+		frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+		frameRate: 10,
+		repeat: -1
+	});
+	
+	this.anims.create({
+		key: 'turn',
+		frames: [ { key: 'dude', frame: 4 } ],
+		frameRate: 20
+	});
+	
+	this.anims.create({
+		key: 'right',
+		frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+		frameRate: 10,
+		repeat: -1
+	});
+
+	player.body.setGravity(300);
 }
 
 function update() {
