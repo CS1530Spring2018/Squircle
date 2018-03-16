52def4f118e7a9640b2cadd421780e67db8e0a93
Tyler Bobik <shed412@gmail.com>
2018-03-15 22:22:30 -0400

    character moves with controller input

--- project/static/scripts/js/testGame.js
+++ project/static/scripts/js/testGame.js
@@ -1,13 +1,23 @@
 //test game code
 
 // dynamically load url?
-var baseUrl = "http://127.0.0.1:5000/static/";
+// pitt ip: 10.215.97.21:5000
+var baseUrl = "http://10.215.100.78:5000/static/";
 
 var assets = {
 	'image': 'images/gameAssets/test/',
 }
 var platforms;
+var inputTimer = 0;
+var receiving = false;
 var player;
+var cursors;
+var drone;
+var xDig;
+var yDig;
+var log;
+
+var drone;
 
 function url_for(type, name) {
 	return baseUrl + assets[type] + name;
@@ -52,6 +62,8 @@ function preload() {
 	*/
 	this.load.spritesheet('dude', url_for('image', 'dude.png',),
 	{frameWidth: 32, frameHeight: 48});
+
+	drone = new ScaleDrone('JX2gIREeJoi7FDzN')
 }
 
 function create() {
@@ -81,7 +93,7 @@ function create() {
 	this.anims.create({
 		key: 'turn',
 		frames: [ { key: 'dude', frame: 4 } ],
-		frameRate: 20
+		frameRate: 5
 	});
 	
 	this.anims.create({
@@ -94,8 +106,117 @@ function create() {
 	player.body.setGravity(300);
 
 	this.physics.add.collider(player, platforms);
+
+	cursors = this.input.keyboard.createCursorKeys();
+
+	drone.on('open', function(error) {
+
+		//checking for errors
+		if(error){
+			return console.error(error);
+		}
+
+		var room = drone.subscribe('my_game');
+
+		room.on('open', function (error) {
+			if (error) {
+				console.error(error);
+			} else {
+				console.log('Connected to room');
+			}
+		});
+
+		room.on('data', function (data) {
+			inputTimer = 0;
+			receiving = true;
+			// Record controller state
+			xDig = data.xdig;
+			yDig = data.ydig;
+			log = data.log;
+		});
+
+	});
+
+	drone.on('error', function(error){
+		console.log(error);
+	});
+}
+
+
+/**
+ * this will be what controls the player and interprets
+ * data from the incoming socket signal
+*/
+
+function playerController() {
+	inputTimer ++;
+	
+	//console.log(log);
+	if(inputTimer > 7) {
+			
+		// inputTimer = 0;
+		// receiving = false;
+		// xDig = 0;
+		// yDig = 0;
+		// log = null;
+	}
+
+	console.log("xdig: " + xDig);
+	if(xDig > 0 && receiving) {
+		moveRight(160);
+		//console.log("TEST");
+	} else if (xDig < 0 && receiving) {
+		moveLeft(160);
+	} else {
+		idle();
+	}
+
+	if(log === 'tapFunction') {
+		jump(500);
+	}
+}
+
+function moveRight(velocity) {
+	player.setVelocityX(velocity);
+	player.anims.play('right', true);
+}
+
+function moveLeft(velocity) {
+	player.setVelocityX(-velocity);
+	player.anims.play('left', true);
+}
+
+function jump(velocity) {
+	player.setVelocityY(-velocity);
+}
+
+function idle() {
+	player.setVelocityX(0);
+	player.anims.play('turn');
 }
 
 function update() {
 
+	// if(cursors.left.isDown && cursors.right.isDown) {
+	// 	idle();
+	// }
+
+	// if (cursors.right.isDown && !cursors.left.isDown){
+	// 	moveRight(160);
+	// }
+
+	// if (cursors.left.isDown && !cursors.right.isDown){
+	// 	moveLeft(160);
+	// }
+	
+	// if(!cursors.left.isDown && !cursors.right.isDown) {
+	// 	idle();
+	// }
+
+	// if (cursors.up.isDown && player.body.touching.down){
+	// 	jump(500);
+	// }
+
+	playerController();
+	
 }
