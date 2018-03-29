e3755a4a154bee468badd25a806f81b4b5d8118b
Luke Gerhart <luke_gerhart@hotmail.com>
2018-03-28 23:36:26 -0400

    some test cases

--- project/tests/routes_test.py
+++ project/tests/routes_test.py
@@ -13,12 +13,58 @@ class RoutesTestCase(unittest.TestCase):
 		self.app = squircle.app.test_client()
 		squircle.db.drop_all()
 		squircle.db.create_all()
-			
+		
 	def tearDown(self):
 		pass
 	
 	def test_index(self):
 		rv = self.app.get('/')
 		assert b'lobby' in rv.data
+
+	def login(self, username, password):
+		return self.app.post('/login/', data={
+			'user':username,
+			'pass':password,
+			'type':'Login'
+		}, follow_redirects=True)
+	
+	def logout(self):
+		return self.app.get('/logout/')
+	
+	def create_account(self, username, password, age, country, about_me):
+		return self.app.post('/login/', data={
+			'user':username,
+			'pass':password,
+			'age':age, 
+			'country':country,
+			'about':about_me,
+			'type':'Create Account'
+		}, follow_redirects=True)
+			
+	def test_create_account(self):
+		rv = self.create_account('admin', 'root', '20', 'usa', 'some about me text')
+		assert b'Join Lobby' in rv.data
+	
+	def test_create_duplicate_account(self):
+		self.create_account('admin', 'root', '20', 'usa', 'some about me text')
+		self.logout()
+		rv = self.create_account('admin', 'root', '20', 'usa', 'some about me text')
+		assert b'Username already exists. Try something different.' in rv.data
+
+	def test_create_account_no_optional_info(self):
+		rv = self.create_account('admin', 'root', '', '', '')
+		assert b'Join Lobby' in rv.data
+	def test_login(self):
+		self.create_account('admin', 'root', '20', 'usa', 'some about me text')
+		self.logout()
+		rv = self.login('admin', 'root')
+		assert b'Join Lobby' in rv.data
+		
+	def test_bad_login_password(self):
+		self.create_account('admin', 'root', '20', 'usa', 'some about me text')
+		self.logout()
+		rv = self.login('admin', 'bad_password')
+		assert b'Invalid login. Try again.' in rv.data
+
 if __name__ == '__main__':
     unittest.main()
