902ebbc46e3cf5c4dfaf2bf12c5d414666295cab
Luke Gerhart <luke_gerhart@hotmail.com>
2018-04-03 13:26:22 -0400

    fix test class

--- project/tests/socks_test.py
+++ project/tests/socks_test.py
@@ -2,28 +2,25 @@ from flask import Flask, current_app
 from flask_socketio import SocketIO
 import os, unittest, sys, json
 sys.path.append('..')
-from vars import room_occupants, room_ready
+import squircle
+from socks import room_occupants, room_ready
 
 class SocksTestCase(unittest.TestCase):
 	
 	def setUp(self):
-		self.app = Flask(__name__)
+		self.app = squircle.app#Flask(__name__)
 		self.app.config['TESTING'] = True
 		self.app.config['DEBUG'] = False
 		self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 		self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-		self.app.secret_key = os.urandom(24)
-		#with self.app.app_context():
-			#self.test_app = current_app.test_client()
-		self.socketio = SocketIO(self.app)
-		self.client = self.socketio.test_client(self.app)
+		self.client = squircle.socketio.test_client(self.app)
 	
 	def tearDown(self):
-		pass
+		room_occupants.clear()
+		room_ready.clear()
 	
 	def test_create(self):
 		self.client.emit('create', {'code':'AAAAA'})
-		print(room_occupants)
 		assert 'AAAAA' in room_occupants
 	
 	def test_create_users_empty(self):
@@ -39,12 +36,9 @@ class SocksTestCase(unittest.TestCase):
 	def test_join_receive_emit(self):
 		# clear old socket.io notifications
 		self.client.get_received()
-		
 		self.client.emit('create', {'code':'AAAAA'})
 		self.client.emit('join', {'code':'AAAAA', 'username':'tester'})
-		
 		rec = self.client.get_received()
-		print(rec)
 		assert len(rec) == 1
 		assert rec[0]['name'] == 'new user'
 
