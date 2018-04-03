865e764eff0ab106507c832685f96eb3ed49a69b
Luke Gerhart <luke_gerhart@hotmail.com>
2018-04-03 14:08:19 -0400

    more unit tests

--- project/tests/socks_test.py
+++ project/tests/socks_test.py
@@ -58,20 +58,52 @@ class SocksTestCase(unittest.TestCase):
 		occupants = room_occupants['AAAAA']
 		assert 'tester' in occupants['players']
 		
+	def make_room_ready(self, roomcode, offset=0):
+		self.client.emit('create', {'code':roomcode})
+		for i in range(num_players-offset):
+			self.client.emit('join', {'code':roomcode, 'username':'player'+str(i)})
+			self.client.emit('ready', {'room':roomcode, 'username':'player'+str(i)})
+		return self.client.get_received()
+		
 	def test_join_spectator(self):
-		self.client.emit('create', {'code':'AAAAA'})
-		for i in range(num_players):
-			self.client.emit('join', {'code':'AAAAA', 'username':'player'+str(i)})
+		self.make_room_ready('AAAAA')
 		self.client.emit('join', {'code':'AAAAA', 'username':'spectator0'})
 		assert 'spectator0' in room_occupants['AAAAA']['spectators']
 		
 	def test_join_players_reached(self):
-		
-		self.client.emit('create', {'code':'AAAAA'})
-		for i in range(num_players-1):
-			self.client.emit('join', {'code':'AAAAA', 'username':'player'+str(i)})
+		self.make_room_ready('AAAAA', offset=1)
 		# clear old socket.io notifications
 		self.client.get_received()
 		self.client.emit('join', {'code':'AAAAA', 'username':'final_player'})
 		rec = self.client.get_received()
 		assert rec[1]['name'] == 'players reached'
+
+	def test_is_room_ready(self):
+		self.client.emit('is room ready', {'code':'AAAAA'})
+		rec = self.client.get_received()
+		assert len(rec) == 0
+		
+	def test_room_is_ready_emits(self):
+		self.make_room_ready('AAAAA')
+		# clear old socket.io notifications
+		self.client.get_received()
+		self.client.emit('is room ready', {'code':'AAAAA'})
+		rec = self.client.get_received()
+		assert len(rec) == 1
+
+	def test_room_is_ready(self):
+		self.make_room_ready('AAAAA')
+		# clear old socket.io notifications
+		self.client.get_received()
+		self.client.emit('is room ready', {'code':'AAAAA'})
+		rec = self.client.get_received()
+		assert rec[0]['name'] == 'room is ready'
+		
+	def test_room_read_emits(self):
+		self.make_room_ready('AAAAA', offset=1)
+		self.client.emit('join', {'code':'AAAAA', 'username':'final_player'})
+		# clear old socket.io notifications
+		self.client.get_received()
+		self.client.emit('ready', {'room':'AAAAA', 'username':'final_player'})
+		rec = self.client.get_received()
+		assert rec[0]['name'] == 'all ready'
