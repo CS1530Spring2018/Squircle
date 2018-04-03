65347eb0abb6ac314915b87d8d41a21423454cc7
Luke Gerhart <luke_gerhart@hotmail.com>
2018-04-03 13:48:02 -0400

    socket unit tests

--- project/tests/socks_test.py
+++ project/tests/socks_test.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO
 import os, unittest, sys, json
 sys.path.append('..')
 import squircle
-from socks import room_occupants, room_ready
+from socks import room_occupants, room_ready, num_players
 
 class SocksTestCase(unittest.TestCase):
 	
@@ -33,21 +33,45 @@ class SocksTestCase(unittest.TestCase):
 		occupants = room_occupants['AAAAA']
 		assert len(occupants['spectators']) == 0
 		
-	def test_join_receive_emit(self):
+	def join_helper(self, username="tester"):
 		# clear old socket.io notifications
 		self.client.get_received()
 		self.client.emit('create', {'code':'AAAAA'})
-		self.client.emit('join', {'code':'AAAAA', 'username':'tester'})
-		rec = self.client.get_received()
+		self.client.emit('join', {'code':'AAAAA', 'username':username})
+		return self.client.get_received()
+	
+	def test_join_emits(self):
+		rec = self.join_helper()
 		assert len(rec) == 1
+	
+	def test_join_receive_emit(self):
+		rec = self.join_helper()
 		assert rec[0]['name'] == 'new user'
-
-	def test_join(self):
-		# clear old socket.io notifications
-		self.client.get_received()
+	
+	def test_join_receive_username(self):
+		username = "tester"
+		rec = self.join_helper(username)
+		assert rec[0]['args'][0] == username
 		
-		self.client.emit('create', {'code':'AAAAA'})
-		self.client.emit('join', {'code':'AAAAA', 'username':'tester'})
+	def test_join(self):
+		self.join_helper()
 		occupants = room_occupants['AAAAA']
 		assert 'tester' in occupants['players']
 		
+	def test_join_spectator(self):
+		self.client.emit('create', {'code':'AAAAA'})
+		for i in range(num_players):
+			self.client.emit('join', {'code':'AAAAA', 'username':'player'+str(i)})
+		self.client.emit('join', {'code':'AAAAA', 'username':'spectator0'})
+		assert 'spectator0' in room_occupants['AAAAA']['spectators']
+		
+	def test_join_players_reached(self):
+		
+		self.client.emit('create', {'code':'AAAAA'})
+		for i in range(num_players-1):
+			self.client.emit('join', {'code':'AAAAA', 'username':'player'+str(i)})
+		# clear old socket.io notifications
+		self.client.get_received()
+		self.client.emit('join', {'code':'AAAAA', 'username':'final_player'})
+		rec = self.client.get_received()
+		assert rec[1]['name'] == 'players reached'
