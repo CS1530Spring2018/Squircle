901e17041c6f6133ed1b7ab7e7c6bb51703862af
Luke Gerhart <luke_gerhart@hotmail.com>
2018-04-05 08:49:33 -0400

    refactor some stuff

--- project/static/scripts/js/chat.js
+++ project/static/scripts/js/chat.js
@@ -1,25 +1,90 @@
-var socket;
+const Message = ({chat, user}) => (
+	<li className={`chat ${user === chat.username ? "right" : "left"}`}>
+		{user !== chat.username
+			&& <img src={chat.img} alt={`${chat.username}'s profile pic`} />
+		}
+		{chat.content}
+	</li>
+);
 
-function sendMessage(e) {
-	var key = e.keyCode || e.which;
-	if (key == 13) {
-		textArea = $("#typing");
-		message = textArea.value;
-		textArea.value = "";
+class Chatroom extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			chats: [//{
+				// username: "username",
+				// content: <p>usernameusernames</p>,
+				// img: "http://i.imgur.com/Tj5DGiO.jpg", // 
+			// }, {
+				// username: "user1",
+				// content: <p>user1</p>,
+				// img: "http://i.imgur.com/Tj5DGiO.jpg",
+			// }, {
+				// username: "user2",
+				// content: <p>user2</p>,
+				// img: "http://i.imgur.com/Tj5DGiO.jpg",
+			// }, {
+				// username: "user3",
+				// content: <p>user3</p>,
+				// img: "http://i.imgur.com/ARbQZix.jpg",
+			//}
+			]
+		};
+
+		this.submitMessage = this.submitMessage.bind(this);
+	}
+
+	componentDidMount() {
+		this.scrollToBot();
+	}
+
+	componentDidUpdate() {
+		this.scrollToBot();
+	}
+
+	scrollToBot() {
+		ReactDOM.findDOMNode(this.refs.chats).scrollTop = ReactDOM.findDOMNode(this.refs.chats).scrollHeight;
+	}
+
+	submitMessage(e) {
 		e.preventDefault();
-		textArea.focus();
-		socket.emit('new message', {"username":username, "room":lobbycode, "message":message});
+
+		this.setState({
+			chats: this.state.chats.concat([{
+				username: "username",
+				content: <p>{ReactDOM.findDOMNode(this.refs.msg).value}</p>,
+				//img: "http://i.imgur.com/Tj5DGiO.jpg",
+			}])
+		}, () => {
+			ReactDOM.findDOMNode(this.refs.msg).value = "";
+		});
+	}
+
+	render() {
+		const username = "username";
+		const { chats } = this.state;
+
+		return (
+			<div className="chatroom">
+				<h3>Squircle</h3>
+				<ul className="chats" ref="chats">
+					{
+						chats.map((chat) => 
+							<Message chat={chat} user={username} />
+						)
+					}
+				</ul>
+				<form className="input" onSubmit={(e) => this.submitMessage(e)}>
+					<input type="text" ref="msg" />
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		);
 	}
 }
-function setup() {
-	socket = io.connect('http://' + document.domain + ':' + location.port);
 	
-	socket.on('connect', function() {
-		socket.emit('join', {'code': lobbycode, 'username':username});
-	});
-	
-	socket.on('new message', function() {
-		
-	}
-}
-window.addEventListener("load", setup, true);
+ReactDOM.render(
+	<Chatroom />,
+	document.getElementById('Chat')
+);
