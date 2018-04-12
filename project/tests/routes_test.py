4e5f7f36f1e0667f7904ce5d5731ac7073609694
Luke Gerhart <luke_gerhart@hotmail.com>
2018-04-12 00:16:40 -0400

    some routes.py unit tests

--- project/tests/routes_test.py
+++ project/tests/routes_test.py
@@ -41,10 +41,19 @@ class RoutesTestCase(unittest.TestCase):
 			'type':'Create Account'
 		}, follow_redirects=True)
 	
+	def get_lobby_code(self):
+		rv = self.app.get('/lobbycode/')
+		return rv.data.decode('ascii').split('/')[2]
+		
 	def test_logout(self):
 		self.create_account('admin', 'root', '20', 'usa', 'some about me text')
 		rv = self.logout()
 		assert b'You have successfully been logged out!' in rv.data
+	
+	def test_logout_not_logged_in(self):
+		rv = self.app.get('/logout/',follow_redirects=True)
+		assert b'Login' in rv.data
+	
 	def test_create_account(self):
 		rv = self.create_account('admin', 'root', '20', 'usa', 'some about me text')
 		assert b'Join Lobby' in rv.data
@@ -58,12 +67,21 @@ class RoutesTestCase(unittest.TestCase):
 	def test_create_account_no_optional_info(self):
 		rv = self.create_account('admin', 'root', '', '', '')
 		assert b'Join Lobby' in rv.data
+	
+	def test_login_page(self):
+		rv = self.app.get('/login/')
+		assert b'Login' in rv.data
 		
 	def test_login(self):
 		self.create_account('admin', 'root', '20', 'usa', 'some about me text')
 		self.logout()
 		rv = self.login('admin', 'root')
 		assert b'Join Lobby' in rv.data
+	
+	def test_login_redirects_to_profile(self):
+		self.create_account('admin', 'root', '', '', '')
+		rv = self.app.get('/login/')
+		assert b'Login' not in rv.data
 		
 	def test_bad_login_password(self):
 		self.create_account('admin', 'root', '20', 'usa', 'some about me text')
@@ -79,6 +97,14 @@ class RoutesTestCase(unittest.TestCase):
 		rv = self.app.get('/profile/admin')
 		self.assertEqual(rv.status_code, 404)
 	
+	def test_profile_page(self):
+		rv = self.app.get('/profile/', follow_redirects=True)
+		assert b'Login' in rv.data
+	
+	def test_username_in_profile_page(self):
+		rv = self.create_account('bob', 'bob', '', '', '')
+		assert b'bob' in rv.data
+	
 	def test_view_other_profile(self):
 		self.create_account('admin', 'root', '20', 'usa', 'some about me text')
 		self.logout()
@@ -91,13 +117,11 @@ class RoutesTestCase(unittest.TestCase):
 		assert b'lobby' in rv.data
 		
 	def test_lobbycode_uppercase(self):
-		rv = self.app.get('/lobbycode/')
-		code = rv.data.decode('ascii').split('/')[2]
+		code = self.get_lobby_code()
 		self.assertTrue(code.isupper())
 		
 	def test_lobbycode_five_digits(self):
-		rv = self.app.get('/lobbycode/')
-		code = rv.data.decode('ascii').split('/')[2]
+		code = self.get_lobby_code()
 		self.assertEqual(len(code), 5)
 		
 	def test_join_bad_lobby(self):
@@ -106,10 +130,46 @@ class RoutesTestCase(unittest.TestCase):
 		assert b'That lobby is not valid!' in rv.data
 		
 	def test_join_valid_lobby(self):
-		rv = self.app.get('/lobbycode/')
-		code = rv.data.decode('ascii').split('/')[2]
+		code = self.get_lobby_code()
 		self.create_account('admin', 'root', '20', 'usa', 'some about me text')
 		rv = self.app.post('/profile/admin', data={'code':code}, follow_redirects=True)
 		assert code in rv.data.decode('ascii')
+	
+	def test_lobby_has_lobbycode(self):	
+		code = self.get_lobby_code()
+		rv = self.app.get('/lobby/'+code)
+		assert bytes(code, 'utf-8') in rv.data
+	
+	def test_faq(self):
+		rv = self.app.get('/help/')
+		assert b'Squircle Help' in rv.data
+	
+	def test_pages_have_help_tips(self):
+		rv = self.app.get('/lobby/')
+		assert b'Help' in rv.data
+		
+		rv = self.app.get('/login/')
+		assert b'Help' in rv.data
+		
+		# /profile/bob
+		rv = self.create_account('bob', 'bob', '', '', '')
+		assert b'Help' in rv.data
+
+		code = self.get_lobby_code()
+		rv = self.app.get('/game/'+code)
+		assert b'Help' in rv.data
+		
+		rv = self.app.get('/lobby/'+code)
+		assert b'Help' in rv.data
+		
+	def test_game_page(self):
+		code = self.get_lobby_code()
+		rv = self.app.get('/game/'+code)
+		assert bytes(code, 'utf-8') in rv.data
+
+	def test_controller_page(self):
+		self.create_account('bob', 'bob', '', '', '')
+		rv = self.app.get('/controller/0')
+		assert b'player_one' in rv.data
 if __name__ == '__main__':
     unittest.main()
